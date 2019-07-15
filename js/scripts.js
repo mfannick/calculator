29263476703a351c9d653fca7580a5d2fe7905c1
mfannick <mfannick1@gmail.com>
2019-07-15 18:56:10 +0200

    addition of css,js,and html files into their respective folders and adding codes in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1,number2){
+    return number1+number2;
+};
+// alert(add(5,6));
+var number1 = parseInt(prompt("enter number: "));
+var number2 = parseInt(prompt("enter another number: ")); 
+
+alert(add(number1,number2));
