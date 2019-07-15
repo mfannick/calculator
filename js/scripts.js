ce8454aa3de75d29878f580fb0c9cb1c02474a26
mfannick <mfannick1@gmail.com>
2019-07-15 19:20:11 +0200

    mistate correction and edition

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ var add = function(number1,number2){
 var number1 = parseInt(prompt("enter number: "));
 var number2 = parseInt(prompt("enter another number: ")); 
 
-alert(add(number1,number2));
+// alert(add(number1,number2));
 
 /*************subtraction function *********************************************************************/
  
@@ -14,15 +14,16 @@ var subtract = function (number1,number2){
     return number1-number2
 
 };
-alert(subtract(number1,number2));
-/*************addition function *********************************************************************/
-var add = function(number1,number2){
-    return number1+number2;
+// alert(subtract(number1,number2));
+/*************division function *********************************************************************/
+var divide = function(number1,number2){
+    return number1/number2;
 };
-alert(add(number1,number2));
+// alert(add(number1,number2));
 /*************multiply function *********************************************************************/
 
 var multiply=function (number1,number2){
     return number1*number2;
 }
-alert(multiply(number1,number2))
+var result= divide(number1,number2);
+alert(result);
