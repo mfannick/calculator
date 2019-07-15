e184687d571520ba7e8231b66770d7312f3542f5
mfannick <mfannick1@gmail.com>
2019-07-15 19:12:25 +0200

    adding multiplication,substraction and division function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
+/************* addition function *********************************************************************/
 var add = function(number1,number2){
     return number1+number2;
 };
-// alert(add(5,6));
+
 var number1 = parseInt(prompt("enter number: "));
 var number2 = parseInt(prompt("enter another number: ")); 
 
 alert(add(number1,number2));
+
+/*************subtraction function *********************************************************************/
+ 
+var subtract = function (number1,number2){
+    return number1-number2
+
+};
+alert(subtract(number1,number2));
+/*************addition function *********************************************************************/
+var add = function(number1,number2){
+    return number1+number2;
+};
+alert(add(number1,number2));
+/*************multiply function *********************************************************************/
+
+var multiply=function (number1,number2){
+    return number1*number2;
+}
+alert(multiply(number1,number2))
